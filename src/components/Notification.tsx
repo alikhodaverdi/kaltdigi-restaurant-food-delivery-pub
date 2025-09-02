de3587b9834f3103f543247fa30effcf32a87719
alikhv <alikhodaveerdi@yahoo.com>
2025-09-02 09:43:32 +0330

    first and initial folder and pages

---
+++ src/components/Notification.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Notification = () => {
+  return (
+    <div className="h-12 bg-red-500 text-center md:text-base cursor-pointer text-sm text-white px-4 flex items-center justify-center">
+      تحویل برای تمام محصولات رایگان است
+    </div>
+  );
+};
+
+export default Notification;
